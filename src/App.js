fb467034d3b3f78f120cb4414b0a1ebb44f16c13
unknown <melek.khabou@enis.tn>
2021-09-20 09:13:16 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import Dialog from "./MUI/Dialog";
+
+export default function App() {
+  return (
+    <div>
+      <Dialog />
+    </div>
+  );
+}
